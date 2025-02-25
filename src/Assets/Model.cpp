5aa54b2d6bf5d0da993092ce8f013d57603cbfad
V2Valdecantos <99716006+V2Valdecantos@users.noreply.github.com>
2025-02-26 03:52:11 +0800

    Update Model.cpp

--- src/Assets/Model.cpp
+++ src/Assets/Model.cpp
@@ -64,146 +64,155 @@ namespace Assets {
 		const std::string materialPath = std::filesystem::path(filename).parent_path().string();
 
 		Assimp::Importer objectImporter;
-		const aiScene* model = objectImporter.ReadFile(filename, 0);
+		const aiScene* scene = objectImporter.ReadFile(filename, aiProcessPreset_TargetRealtime_MaxQuality);
 		// read file and return an aiScene containing model attributes
 
-		if (model == nullptr)
+		if (scene == nullptr)
 		{
 			Throw(std::runtime_error("failed to load model '" + filename + "':\n" + objectImporter.GetErrorString()));
 		}
-
-	// Materials
-	std::vector<Material> materials;
-
-	if (model->HasMaterials()) 
-	{
-		for (int i = 0; i < model->mNumMaterials; i++) 
-		{
-			Material m{};
-
-			aiColor4D diffuse;
-			model->mMaterials[i]->Get(AI_MATKEY_COLOR_DIFFUSE, diffuse);
-		
-			//m.Diffuse.r = diffuse[0];
-			//m.Diffuse.g = diffuse[1];
-			//m.Diffuse.b = diffuse[2];
-			//m.Diffuse.a = 1.0f;
-			
-			m.Diffuse = vec4(diffuse[0], diffuse[1], diffuse[2], 1.0);
-		
-			m.DiffuseTextureId = -1;
-
-			materials.emplace_back(m);
-		}
-	}	
-	else
-	{
-		Material m{};
-
-		m.Diffuse = vec4(0.7f, 0.7f, 0.7f, 1.0);
-		m.DiffuseTextureId = -1;
-
-		materials.emplace_back(m);
-	}
-
-		// Geometry
 		std::string name;
+		name = scene->mName.C_Str();
 		int totalvertices = 0;
-		for (int i = 0; i < model->mNumMeshes; i++)
+
+		std::vector<Material> materials;
+		aiColor4D diffuse;
+		Material material{};
+
+		for (int i = 0; i < scene->mNumMeshes; i++)
 		{
-			totalvertices += model->mMeshes[i]->mNumVertices;
+			totalvertices += scene->mMeshes[i]->mNumVertices;
 		}
 		std::vector<Vertex> vertices;
 		std::vector<uint32_t> indices;
 		std::unordered_map<Vertex, uint32_t> uniqueVertices(totalvertices);
 		size_t faceId = 0;
-
-		for (int m = 0; m < model->mNumMeshes; m++)
+		for (int m = 0; m < scene->mNumMeshes; m++)
 		{
-			name = model->mName.C_Str();
-			for (int v = 0; v < model->mMeshes[m]->mNumVertices; v++)
+
+
+			//Materials and Texture
+			if (AI_SUCCESS != scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->Get(AI_MATKEY_COLOR_DIFFUSE, diffuse))
 			{
-				Vertex vertex = {};
 
-				vertex.Position = {
-					model->mMeshes[m]->mVertices[v].x,
-					model->mMeshes[m]->mVertices[v].y,
-					model->mMeshes[m]->mVertices[v].z,
-				};
+				material.Diffuse = vec4(0.7f, 0.7f, 0.7f, 1.0);
+				material.DiffuseTextureId = -1;
 
-				if (model->mMeshes[m]->HasNormals())
-				{
-					vertex.Normal = {
-						model->mMeshes[m]->mNormals[v].x,
-						model->mMeshes[m]->mNormals[v].y,
-						model->mMeshes[m]->mNormals[v].z,
-					};
+				std::cout << "No Texture in Mesh!" << std::endl;
+
+			}
+			else
+			{
+				material.Diffuse = vec4(diffuse.r, diffuse.g, diffuse.b, diffuse.a);
+
+				int texcount = scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetTextureCount(aiTextureType_DIFFUSE);
+
+				if (texcount > 0) {
+					aiString texture_file;
+					scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->Get(AI_MATKEY_TEXTURE(aiTextureType_DIFFUSE, 0), texture_file);
+					std::string texName = scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str();
+					if (!TextureLibrary::getInstance()->doesTextureExist(texName))
+					{
+						TextureLibrary::getInstance()->addTexture(texName, FileUtils::getAssetsFolderPath().generic_string() + "/models/" + texture_file.C_Str());
+						std::cout << "Initialized Texture " << texName << std::endl;
+					}
+
+					material.DiffuseTextureId = TextureLibrary::getInstance()->getTextureId(scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str());
+
 				}
 				else
 				{
-					// Compute a normalized vector from the vertex itself.
-					auto norm = vec3(model->mMeshes[m]->mVertices[v].x,
-						model->mMeshes[m]->mVertices[v].y,
-						model->mMeshes[m]->mVertices[v].z);
-					vertex.Normal = normalize(norm);
+					material.Diffuse = vec4(0.7f, 0.7f, 0.7f, 1.0);
+					material.DiffuseTextureId = -1;
+
+					std::cout << "No Texture in Material: " << scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str() << std::endl;
 				}
 
-				if (model->mMeshes[m]->HasTextureCoords(0))
+
+			}
+
+			materials.emplace_back(material);
+
+			// Geometry
+			for (int f = 0; f < scene->mMeshes[m]->mNumFaces; f++) 
+			{
+
+				for (int i = 0; i < scene->mMeshes[m]->mFaces[f].mNumIndices; i++)
 				{
-					vertex.TexCoord = {
-						model->mMeshes[m]->mTextureCoords[0][v].x,
-						1 - model->mMeshes[m]->mTextureCoords[0][v].y
+
+					Vertex vertex = {};
+					int v = scene->mMeshes[m]->mFaces[f].mIndices[i];
+
+					vertex.Position =
+					{
+						scene->mMeshes[m]->mVertices[v].x,
+						scene->mMeshes[m]->mVertices[v].y,
+						scene->mMeshes[m]->mVertices[v].z,
 					};
-				}
 
-				vertex.MaterialIndex = model->mMeshes[m]->mMaterialIndex;
+					if (scene->mMeshes[m]->HasNormals())
+					{
+						vertex.Normal =
+						{
+							scene->mMeshes[m]->mNormals[v].x,
+							scene->mMeshes[m]->mNormals[v].y,
+							scene->mMeshes[m]->mNormals[v].z,
+						};
+					}
+					else
+					{
+						vertex.Normal =
+						{
+							scene->mMeshes[m]->mVertices[v].Normalize().x,
+							scene->mMeshes[m]->mVertices[v].Normalize().y,
+							scene->mMeshes[m]->mVertices[v].Normalize().z,
+						};
+					}
 
-				if (uniqueVertices.count(vertex) == 0)
-				{
+					if (scene->mMeshes[m]->HasTextureCoords(0))
+					{
+						vertex.TexCoord =
+						{
+							(float)scene->mMeshes[m]->mTextureCoords[0][v].x,
+							(float)scene->mMeshes[m]->mTextureCoords[0][v].y
+						};
+					}
+
+					//vertex.MaterialIndex = std::max(0, mesh.material_ids[faceId++ / 3]);
+
+					vertex.MaterialIndex = scene->mMeshes[m]->mMaterialIndex;
+
 					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
 					vertices.push_back(vertex);
+
+
+					indices.push_back(uniqueVertices[vertex]);
 				}
 
-				indices.push_back(uniqueVertices[vertex]);
 			}
+
+			if (name == "")
+				name = "Imported Object";
 		}
 
-		// If the model did not specify normals, then create smooth normals that conserve the same number of vertices.
-		if (!model->mMeshes[0]->HasNormals())
-		{
-			for (size_t i = 0; i < indices.size(); i += 3)
-			{
-				const auto normal = normalize(cross(
-					vec3(vertices[indices[i + 1]].Position) - vec3(vertices[indices[i]].Position),
-					vec3(vertices[indices[i + 2]].Position) - vec3(vertices[indices[i]].Position)));
 
-				vertices[indices[i + 0]].Normal += normal;
-				vertices[indices[i + 1]].Normal += normal;
-				vertices[indices[i + 2]].Normal += normal;
-			}
-			for (auto& vertex : vertices)
-			{
-				vertex.Normal = normalize(vertex.Normal);
-			}
-		}
+		//// --- Centering the model at (0,0,0) ---
+		//// Compute bounding box (min and max points)
+		//vec3 minPos(FLT_MAX);
+		//vec3 maxPos(-FLT_MAX);
+		//for (const auto& vertex : vertices)
+		//{
+		//	minPos = glm::min(minPos, vertex.Position);
+		//	maxPos = glm::max(maxPos, vertex.Position);
+		//}
+		//vec3 center = (minPos + maxPos) * 0.5f;
 
-		// --- Centering the model at (0,0,0) ---
-		// Compute bounding box (min and max points)
-		vec3 minPos(FLT_MAX);
-		vec3 maxPos(-FLT_MAX);
-		for (const auto& vertex : vertices)
-		{
-			minPos = glm::min(minPos, vertex.Position);
-			maxPos = glm::max(maxPos, vertex.Position);
-		}
-		vec3 center = (minPos + maxPos) * 0.5f;
-
-		// Shift all vertices so that the model is centered at the origin.
-		for (auto& vertex : vertices)
-		{
-			vertex.Position -= center;
-		}
-		// --- End centering ---
+		//// Shift all vertices so that the model is centered at the origin.
+		//for (auto& vertex : vertices)
+		//{
+		//	vertex.Position -= center;
+		//}
+		//// --- End centering ---
 
 		const auto elapsed = std::chrono::duration<float, std::chrono::seconds::period>(std::chrono::high_resolution_clock::now() - timer).count();
 
@@ -234,11 +243,6 @@ std::vector<Model> Model::LoadModelGroup(const std::string& filename)
 	// Geometry
 	std::string name = "";
 	int totalvertices = 0;
-	//for (int i = 0; i < scene->mNumMeshes; i++)
-	//{
-	//	totalvertices += scene->mMeshes[i]->mNumVertices;
-	//}
-
 	size_t faceId = 0;
 	int texlibcount = TextureLibrary::getInstance()->getTextureLibraryList().size();
 
@@ -249,35 +253,46 @@ std::vector<Model> Model::LoadModelGroup(const std::string& filename)
 		std::vector<uint32_t> indices;
 		std::unordered_map<Vertex, uint32_t> uniqueVertices(scene->mMeshes[m]->mNumVertices);
 
-		//Materials
+		//Materials and Texture
 		std::vector<Material> materials;
 		aiColor4D diffuse;
 		Material material{};
 		if (AI_SUCCESS != scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->Get(AI_MATKEY_COLOR_DIFFUSE, diffuse))
 		{
-			
+
 			material.Diffuse = vec4(0.7f, 0.7f, 0.7f, 1.0);
 			material.DiffuseTextureId = -1;
 
 			std::cout << "No Texture in Mesh!" << std::endl;
-			
+
 		}
-		else 
+		else
 		{
 			material.Diffuse = vec4(diffuse.r, diffuse.g, diffuse.b, diffuse.a);
-			material.DiffuseTextureId = -1;
 
 			int texcount = scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetTextureCount(aiTextureType_DIFFUSE);
 
 			if (texcount > 0) {
 				aiString texture_file;
 				scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->Get(AI_MATKEY_TEXTURE(aiTextureType_DIFFUSE, 0), texture_file);
-
-				if (!TextureLibrary::getInstance()->doesTextureExist(scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str()))
-					TextureLibrary::getInstance()->addTexture(scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str(), FileUtils::getAssetsFolderPath().generic_string() + "/models/" + texture_file.C_Str());	
+				std::string texName = scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str();
+				if (!TextureLibrary::getInstance()->doesTextureExist(texName))
+				{
+					TextureLibrary::getInstance()->addTexture(texName, FileUtils::getAssetsFolderPath().generic_string() + "/models/" + texture_file.C_Str());
+					std::cout << "Initialized Texture " << texName << std::endl;
+				}
 
 				material.DiffuseTextureId = TextureLibrary::getInstance()->getTextureId(scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str());
+
 			}
+			else
+			{
+				material.Diffuse = vec4(0.7f, 0.7f, 0.7f, 1.0);
+				material.DiffuseTextureId = -1;
+
+				std::cout << "No Texture in Material: " << scene->mMaterials[scene->mMeshes[m]->mMaterialIndex]->GetName().C_Str() << std::endl;
+			}
+
 
 		}
 
@@ -334,35 +349,33 @@ std::vector<Model> Model::LoadModelGroup(const std::string& filename)
 				uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
 				vertices.push_back(vertex);
 
-				
+
 				indices.push_back(uniqueVertices[vertex]);
 			}
 
 		}
 
-		//if (!scene->mMeshes[m]->HasNormals())
-		//{
-		//	std::vector<vec3> normals(vertices.size());
-
-		//	for (size_t i = 0; i < indices.size(); i += 3)
-		//	{
-		//		const auto normal = normalize(cross(
-		//			vec3(vertices[indices[i + 1]].Position) - vec3(vertices[indices[i]].Position),
-		//			vec3(vertices[indices[i + 2]].Position) - vec3(vertices[indices[i]].Position)));
-
-		//		vertices[indices[i + 0]].Normal += normal;
-		//		vertices[indices[i + 1]].Normal += normal;
-		//		vertices[indices[i + 2]].Normal += normal;
-		//	}
-
-		//	for (auto& vertex : vertices)
-		//	{
-		//		vertex.Normal = normalize(vertex.Normal);
-		//	}
-		//}
-
 		if (name == "")
-			name = "Sponza_" + m;
+			name = "Object_" + m;
+
+		//// --- Centering the model at (0,0,0) ---
+		//// Compute bounding box (min and max points)
+		//vec3 minPos(FLT_MAX);
+		//vec3 maxPos(-FLT_MAX);
+		//for (const auto& vertex : vertices)
+		//{
+		//	minPos = glm::min(minPos, vertex.Position);
+		//	maxPos = glm::max(maxPos, vertex.Position);
+		//}
+		//vec3 center = (minPos + maxPos) * 0.5f;
+
+		//// Shift all vertices so that the model is centered at the origin.
+		//for (auto& vertex : vertices)
+		//{
+		//	vertex.Position -= center;
+		//}
+		//// --- End centering ---
+
 
 		Model model = Model(name, std::move(vertices), std::move(indices), std::move(materials), nullptr);
 		models.push_back(model);
