30cca1183dec6f46e4abcdd6ace3d5b72d6aa34f
V2Valdecantos <99716006+V2Valdecantos@users.noreply.github.com>
2025-06-10 17:37:18 +0800

    minor model changes

--- src/Assets/Model.cpp
+++ src/Assets/Model.cpp
@@ -65,7 +65,9 @@ namespace Assets {
 		const std::string materialPath = std::filesystem::path(filename).parent_path().string();
 
 		Assimp::Importer objectImporter;
-		const aiScene* scene = objectImporter.ReadFile(filename, aiProcess_FlipUVs | aiProcess_Triangulate | 0);
+		const aiScene* scene = objectImporter.ReadFile(filename, aiProcess_CalcTangentSpace | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices |
+			aiProcess_ImproveCacheLocality | aiProcess_LimitBoneWeights | aiProcess_SplitLargeMeshes | aiProcess_Triangulate |
+			aiProcess_GenUVCoords | aiProcess_SortByPType | aiProcess_FindInvalidData | aiProcess_ValidateDataStructure | aiProcess_FlipUVs | 0);
 		// read file and return an aiScene containing model attributes
 
 		if (scene == nullptr)
@@ -237,7 +239,9 @@ namespace Assets {
 		Assimp::Importer objectImporter;
 		std::vector<Model> models;
 
-		const aiScene* scene = objectImporter.ReadFile(filename, aiProcess_FlipUVs | aiProcess_Triangulate | 0); //read file and return an aiScene containing model attributes
+		const aiScene* scene = objectImporter.ReadFile(filename, aiProcess_CalcTangentSpace | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices |
+			aiProcess_ImproveCacheLocality | aiProcess_LimitBoneWeights | aiProcess_SplitLargeMeshes | aiProcess_Triangulate |
+			aiProcess_GenUVCoords | aiProcess_SortByPType | aiProcess_FindInvalidData | aiProcess_ValidateDataStructure | aiProcess_FlipUVs | 0); //read file and return an aiScene containing model attributes
 
 
 		if (scene == nullptr)
