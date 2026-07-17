fe53f3b4d8ad097b1be5234c742901ad40c71487
JR Cala <jrcala7@gmail.com>
2026-07-17 15:31:51 +0800

    Project setup

---
+++ AnitoTracerRebuild/AnitoTracerRebuild.cpp
@@ -0,0 +1,12 @@
+﻿// AnitoTracerRebuild.cpp : Defines the entry point for the application.
+//
+
+#include "AnitoTracerRebuild.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
