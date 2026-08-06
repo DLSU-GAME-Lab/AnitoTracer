6e6be32a808b1d572360ae404722505cc259790d
Fonzy Fermin <fonzy.fermin@gmail.com>
2026-08-07 00:25:46 +0800

    Separate body and engine interfaces

---
+++ src/Physics/IPhysicsBody.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+class IPhysicsBody {
+public:
+	virtual ~IPhysicsBody() = default;
+
+	// Transform
+	virtual void SetPosition(const glm::vec3& position) = 0;
+	virtual glm::vec3 GetPosition() const = 0;
+
+	virtual void SetRotation(const glm::quat& rotation) = 0;
+	virtual glm::quat GetRotation() const = 0;
+
+	// Phsyics properties
+	virtual void SetMass(float mass) = 0;
+	virtual float GetMass() const = 0;
+
+	virtual void SetVelocity(const glm::vec3& velocity) = 0;
+	virtual glm::vec3 GetVelocity() const = 0;
+
+	virtual void SetAngularVelocity(const glm::vec3& angularVelocity) = 0;
+	virtual glm::vec3 GetAngularVelocity() const = 0;
+
+	// Forces
+	virtual void ApplyForce(const glm::vec3& force) = 0;
+	virtual void ApplyImpulse(const glm::vec3& impulse) = 0;
+};
--- src/Physics/IPhysicsEngine.hpp
+++ src/Physics/IPhysicsEngine.hpp
@@ -1,14 +1,29 @@
 #pragma once
 
+#include "IPhysicsBody.hpp"
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include <memory>
 #include <vector>
-
-class IPhysicsBody;
+#include <functional>
 
 class IPhysicsEngine {
 public:
 	virtual ~IPhysicsEngine() = default;
+
+	enum class ShapeType {
+		Box,
+		Sphere,
+		Capsule
+	};
+
+	// Generic shape parameters
+	struct ShapeParams {
+		glm::vec3 v = glm::vec3(1.0f); // For box: half extents, for sphere: radius in x, for capsule: radius in x and height in y
+	};
+
+	// Collision callback
+	using CollisionCallback = std::function<void(std::shared_ptr<IPhysicsBody>, std::shared_ptr<IPhysicsBody>, const glm::vec3&)>;
 
 	// World management
 	virtual void SetGravity(const glm::vec3& gravity) = 0;
@@ -18,39 +33,18 @@ public:
 	virtual std::shared_ptr<IPhysicsBody> CreateRigidBody(
 		const glm::vec3& position,
 		const glm::quat& rotation,
-		float mass
+		float mass,
+		ShapeType shape = ShapeType::Box,
+		const ShapeParams& shapeParams = {}
 	) = 0;
 	virtual void DestroyRigidBody(std::shared_ptr<IPhysicsBody> body) = 0;
+
+	// Collision callback
+	virtual void SetCollisionCallback(CollisionCallback callback) = 0;
 
 	// Simulation
 	virtual void Step(float deltaTime) = 0;
 
 	// Ray casting
 	virtual bool Raycast(const glm::vec3& origin, const glm::vec3& direction, float maxDistance, std::shared_ptr<IPhysicsBody>& outBody, glm::vec3& outHitPoint) = 0;
-};
-
-class IPhysicsBody {
-public:
-	virtual ~IPhysicsBody() = default;
-
-	// Transform
-	virtual void SetPosition(const glm::vec3& position) = 0;
-	virtual glm::vec3 GetPosition() const = 0;
-
-	virtual void SetRotation(const glm::quat& rotation) = 0;
-	virtual glm::quat GetRotation() const = 0;
-
-	// Phsyics properties
-	virtual void SetMass(float mass) = 0;
-	virtual float GetMass() const = 0;
-
-	virtual void SetVelocity(const glm::vec3& velocity) = 0;
-	virtual glm::vec3 GetVelocity() const = 0;
-
-	virtual void SetAngularVelocity(const glm::vec3& angularVelocity) = 0;
-	virtual glm::vec3 GetAngularVelocity() const = 0;
-
-	// Forces
-	virtual void ApplyForce(const glm::vec3& force) = 0;
-	virtual void ApplyImpulse(const glm::vec3& impulse) = 0;
 };
