eace0d29d4c44407461001cf134bb53d4aef37da
Cybalt-SR <gabriel.paulo.rayo@gmail.com>
2026-08-11 17:06:36 +0800

    Fix missing template build error for persistent references

--- src/Objects/Components/Camera.hpp
+++ src/Objects/Components/Camera.hpp
@@ -57,12 +57,12 @@ private:
     float m_FarZ = 1000.0f;
     GBE_SERIALIZE_FIELD_W_NAME(m_FarZ, "Far Plane");
 
-    gbe::IInstanceManager<HierarchyObject>::Ref m_lookAtObj = nullptr;
-    Transform* m_lookAtTransform = nullptr;
+    gbe::ObjectRef<HierarchyObject> m_lookAtObj = nullptr;
+    gbe::ObjectRef<Transform> m_lookAtTransform = nullptr;
     //TODO: This breaks the build due to missing serializer definition
     // Pls fix
-    //GBE_SERIALIZE_FIELD_W_NAME(m_lookAtObj, "Look at Object");
-    //GBE_SERIALIZE_FIELD_W_NAME(m_lookAtTransform, "Look at Transform");
+    GBE_SERIALIZE_FIELD_W_NAME(m_lookAtObj, "Look at Object");
+    GBE_SERIALIZE_FIELD_W_NAME(m_lookAtTransform, "Look at Transform");
     
 
     glm::mat4 m_ViewMatrix = glm::mat4(1.0f);  
